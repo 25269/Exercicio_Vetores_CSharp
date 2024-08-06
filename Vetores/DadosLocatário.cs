e2208fa7650682af86fa75df61548b6717c5104a
Cassiano Baldasso <baldassocassiano@gmail.com>
2024-08-06 14:43:38 -0300

    Primeira Versão

---
+++ Vetores/DadosLocatário.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    internal class DadosLocatário
+    {
+        public string NomeLocatario { get; set; }
+        public string EmailLocatario { get; set; }
+        public int NumeroQuarto { get; set; }
+    }
+}
---
+++ Vetores/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DadosLocatário[] vetDadosAluguel = new DadosLocatário[10];
+
+            int qtdAlugueis = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdAlugueis; i++)
+            {
+                Console.Write("Digite o seu nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Digite o seu e-mail: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Digite o número do quarto: ");
+                int numeroQuarto = int.Parse(Console.ReadLine());
+
+                vetDadosAluguel[numeroQuarto] = new DadosLocatário {NomeLocatario = nome, EmailLocatario = email, NumeroQuarto = numeroQuarto };
+
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < vetDadosAluguel.Length; i++)
+            {
+                if (vetDadosAluguel[i] != null)
+                {
+                    Console.WriteLine(vetDadosAluguel[i].NumeroQuarto + ": " + vetDadosAluguel[i].NomeLocatario + ", " + vetDadosAluguel[i].EmailLocatario);
+                }
+            }
+
+
+        }
+    }
+}
